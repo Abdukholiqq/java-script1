465bc58b7260a382a1763036cdfca4b453aff6cb
Abdukholiqq <sumusddollar@gmail.com>
2022-11-16 22:29:07 +0500

    homework1

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//1
+var a = 10;
+var b =5;
+ 
+console.log(a + b);
+
+//2
+var a = 25;
+var b =15;
+var result = a - b;
+ 
+console.log(result);
+
+
+//3
+var a = 10;
+var b =5;
+var c = a - b;
+var d = 7;
+var result = c + d;
+console.log(result);
+
+
+//4
+var a = 10;
+var b =3;
+ 
+console.log(a % b);
+
+
+
+//5
+var a = 2;
+ 
+console.log(a ** 10);
+
+
+//Amaliyot
+
+
+var userName = prompt("ismingizni kiriting");
+var surname = prompt("familiyangiz");
+var userAge = prompt("Yoshingiz");
+var study = prompt("Qayerda o'qigansiz (o'qiysiz)");
+var work = prompt("qayerda ishlagansiz");
+var favoritecolor = prompt("yoqtirgan rangingiz");
+var future = prompt("kelajakdagi orzuingiz");
+var goal = prompt("maqsadingiz");
+
+
+
+console.log(`Mening ismim ${userName}. Familiyam ${surname} 
+.Yoshim ${userAge} da. Men ${study} da o'qiganman va ${work} da ishlayman(ishlaganman). 
+Men ${favoritecolor} rangni yaxshi ko'raman.  Kelajakda eng salohiyatli ${future}. Maqsadim ${goal}. `);
